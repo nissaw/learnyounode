5d17a3b3008101774178d7b1ae90ea9132d99ab0
nissaw <nissa.wollum@gmail.com>
2016-03-14 23:38:07 -0700

    Completed Lesson 8

--- learnyounode/script.js
+++ learnyounode/script.js
@@ -67,14 +67,47 @@ console.log(getNewLines());*/
 // });  
 
 //LESSON 7
-var http = require('http');
+// var http = require('http');
 
-var url = process.argv[2];
+// var url = process.argv[2];
 
-http.get(url, function(res){
-  res.setEncoding('utf8');
-  res.on('data', console.log);
-  res.on('error', console.error);
-});
+// http.get(url, function(res){
+//   res.setEncoding('utf8');
+//   res.on('data', console.log);
+//   res.on('error', console.error);
+// });
 
+// LESSON 8
+// var http = require('http');
 
+// var url = process.argv[2];
+
+// http.get(url, function(res){
+//   var str = '';
+//   res.setEncoding('utf8');
+//   res.on('data', function(data){
+//     str+=data;
+//   });
+//   res.on('end', function(){
+//     console.log(str.length);
+//     console.log(str);
+//   });
+//   res.on('error', console.error);
+// });
+
+/* USING BUFFER-LIST  CONCAT-STREAM ANOTHER OPTION
+
+var http = require('http')  
+var bl = require('bl')  
+  
+http.get(process.argv[2], function (response) {  
+  response.pipe(bl(function (err, data) {  
+    if (err)  
+      return console.error(err)  
+    data = data.toString()  
+    console.log(data.length)  
+    console.log(data)  
+  }))    
+})  */
+
+//LESSON 9
