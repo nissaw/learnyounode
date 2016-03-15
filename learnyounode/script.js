bf5a8abe6d3eaee6a055357b9b7b1cdcab24b848
nissaw <nissa.wollum@gmail.com>
2016-03-14 23:26:53 -0700

    Completed Lesson 7

--- learnyounode/script.js
+++ learnyounode/script.js
@@ -53,15 +53,28 @@ console.log(getNewLines());*/
 // });
 
 //LESSON 6
-var mod = require('./mod.js');
+// var mod = require('./mod.js');
 
-// the list passed here has already been filtered for ext
-mod(process.argv[2], process.argv[3], function(err, list){
-  if (err){
-    console.log('There was an error: ', err);
-    return err;
-  } 
-  list.forEach(function(file){
-    console.log(file);
-  });
-});  
+// // the list passed here has already been filtered for ext
+// mod(process.argv[2], process.argv[3], function(err, list){
+//   if (err){
+//     console.log('There was an error: ', err);
+//     return err;
+//   } 
+//   list.forEach(function(file){
+//     console.log(file);
+//   });
+// });  
+
+//LESSON 7
+var http = require('http');
+
+var url = process.argv[2];
+
+http.get(url, function(res){
+  res.setEncoding('utf8');
+  res.on('data', console.log);
+  res.on('error', console.error);
+});
+
+
